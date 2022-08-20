4c76c317107c6e1bbd50735628d8f01a6df65e9c
Justin Sun <i@jsun969.cn>
2022-08-20 15:14:30 +0800

    refactor: move `queryClient` to `/lib`

--- src/providers/query.tsx
+++ src/providers/query.tsx
@@ -1,6 +1,6 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 
-export const queryClient = new QueryClient();
+import { queryClient } from '@/lib/query';
 
 const QueryProvider = ({ children }: { children: React.ReactNode }) => {
   return (
